a6c0730e7f56c63b80e2d48ddd76bae38857fbdf
Karl Ward <karl@ghostghost.net>
2014-04-30 12:49:30 -0400

    added info about import --KW

--- html/tiPage.php
+++ html/tiPage.php
@@ -3,11 +3,16 @@
 $export_json = '';
 $export = array();
 if (get_option('ti_export_url')) {
-  echo 'importing from ' . get_option('ti_export_url') . '... ';
+  echo 'Importing from ' . get_option('ti_export_url') . '... ';
   $export_json = file_get_contents(get_option('ti_export_url'));
   $export = json_decode($export_json);
   if (count($export) > 0) {
     echo ' retrieved ' . count($export) . ' theses.';
+    echo '<ul>\n';
+    foreach ($export as $t) {
+      echo '<li>' . $t['preferred_name'] . ': ' . $t['title'] . '</li>\n';
+    }
+    echo '</ul>\n';
   }
   else {
     echo 'import failed';
@@ -17,5 +22,6 @@ if (get_option('ti_export_url')) {
 else {
   echo "ti_export_url not set";
 }
-
 ?>
+
+
