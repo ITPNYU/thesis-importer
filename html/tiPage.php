27a48c1a7aa875acef96898025ed5150f143c691
Karl Ward <karl@ghostghost.net>
2014-04-30 12:54:41 -0400

    debugging --KW

--- html/tiPage.php
+++ html/tiPage.php
@@ -5,13 +5,12 @@ $export = array();
 if (get_option('ti_export_url')) {
   echo 'Importing from ' . get_option('ti_export_url') . '... ';
   $export_json = file_get_contents(get_option('ti_export_url'));
-  $export = json_decode($export_json);
+  $export = json_decode($export_json, TRUE);
   if (count($export) > 0) {
     echo ' retrieved ' . count($export) . ' theses.';
-    echo '<ul>\n';
+    echo "<ul>\n";
     foreach ($export as $t) {
-      #echo '<li>' . $t['preferred_name'] . ': ' . $t['title'] . '</li>\n';
-      echo "<li>" . $t['preferred_name'] . "</li>\n";
+      echo '<li>' . $t['preferred_name'] . ': ' . $t['title'] . "</li>\n";
     }
     echo "</ul>\n";
   }
