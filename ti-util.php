44865a64f59e73b6493f82e54a381ef0e9b0dee0
Karl Ward <karl@ghostghost.net>
2014-04-30 14:38:54 -0400

    category logic added --KW

--- ti-util.php
+++ ti-util.php
@@ -14,16 +14,29 @@ function ti_format_content($s) {
 function ti_post($s, $p) {
   $post_id = null;
   if (isset($p)) {
+    $post_id = $p;
     
   }
   else {
-    $post_id = wp_insert_post( array(
-      'post_title' => $s['thesis']['title'],
-      'post_name' => sanitize_title($s['preferred_name'] . '-' . $s['thesis']['title']),
-      'post_status' => 'publish',
-      'post_content' => ti_format_content($s) #,
-      #'post_category' => array(),
-    ) );
+    $advisor_cat = get_category_by_slug(sanitize_title($s['advisor']));
+    if ($advisor_cat == false) {
+      $advisor_cat = wp_insert_category(
+        array(
+          'cat_name' => $s['advisor'],
+          'category_nicename' => sanitize_title($s['advisor'])
+        )
+      );
+    }
+
+    $post_id = wp_insert_post(
+      array(
+        'post_title' => $s['thesis']['title'],
+        'post_name' => sanitize_title($s['preferred_name']),
+        'post_status' => 'publish',
+        'post_content' => ti_format_content($s),
+        'post_category' => array($advisor_cat->term_id),
+      )
+    );
   } 
   return $post_id;
 }
