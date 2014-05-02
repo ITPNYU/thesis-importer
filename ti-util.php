b95cbb60c74687e4b033f203785185ef6f89f3df
Karl Ward <karl@ghostghost.net>
2014-05-02 14:16:04 -0400

    formatting change --KW

--- ti-util.php
+++ ti-util.php
@@ -6,8 +6,8 @@ function ti_format_content($s) {
   if (isset($s['thesis']['link'])) {
     $thesis_content .= $s['thesis']['link'] . "\n";
   }
-  if (isset($s['thesis']['image'])) {
-    $thesis_content .= $s['thesis']['image'];
+  if (isset($s['thesis']['image']) && ('' != $s['thesis']['image'])) {
+    $thesis_content .= '<img src="' . $s['thesis']['image'] . '" width="80%" />';
   }
   if (isset($s['thesis']['description'])) {
     $thesis_content .= "<h3>Description</h3>\n" . $s['thesis']['description'];
